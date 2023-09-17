2c2e7c02cb96c5622533c4165abe7524551d80ad
unbigou <111555933+unbigou@users.noreply.github.com>
2023-09-17 11:23:36 -0300

    feat: normalizer

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Rodape from '../components/rodape'
 import Cabecalho from '../components/cabecalho'
+import Normalizar from '../components/normalizar'
 
 export default function Home() {
   return (
-    <>
-    <Cabecalho />
-    <h1>teste</h1>
-    <Rodape/>
-    </>
-      )
+        <>
+        <Normalizar/>
+        <Cabecalho />
+            <h1>teste</h1>
+        <Rodape/>
+        </>
+    )
 
 }
