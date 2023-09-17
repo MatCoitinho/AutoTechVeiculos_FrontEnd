ae95149c209417144b1ea571e35ef59a54dd4fca
jpAndrade23 <joaopedro917382@gmail.com>
2023-09-16 23:09:13 -0300

    feat: construção dos componentes para consulta de veiculos pronta

--- components/informacoesVeiculo.js
+++ components/informacoesVeiculo.js
@@ -1,59 +1,26 @@
 
 import styles from '../styles/informacoesVeiculos.module.css'
-export default function informacoesVeiculos(){
-    const autoVeiculo =  {
-        "id": 1,
-        "placa": "ajo4123",
-        "quilometragem": "0",
-        "status": true,
-        "preco": 4200000,
-        "veiculo": 1,
-        "servico": false,
-        "dono": 2,
-        "cor": "Preto"
-    };
-    const autoModelo = {
-        "id": 1,
-        "tipoCombustivel": "Flex",
-        "model": "Onix ltz",
-        "marca": "Chevrolete",
-        "ano": "2023-09-16",
-        "cambio": true,
-        "categoria": "Sedan",
-        "qtdPortas": 4
-    };
+export default function informacoesVeiculos(obj){
+    const autoModelo = obj.value.modelo;
+    const autoVeiculo = obj.value.veiculo;
+    const donoAuto = obj.value.dono;
 
-    const donoAuto = {
-        "id": 1,
-        "password": "pbkdf2_sha256$600000$yg8jsHKgGgcpZlrglwC8V7$BQGHkpvPsV4MUn97vlNmgSwW53veIt6JGLc8ig/m+Ts=",
-        "last_login": "2023-09-13T23:17:54.013093-03:00",
-        "is_superuser": true,
-        "username": "admin",
-        "first_name": "Matheus",
-        "last_name": "Coito",
-        "email": "admin@example.com",
-        "is_staff": true,
-        "is_active": true,
-        "date_joined": "2023-09-13T23:14:07.111780-03:00",
-        "groups": [],
-        "user_permissions": []
-    }
-   
+    
 
     let servico;
-    if(autoVeiculo.servico == true){
+    if(autoVeiculo.servico === true){
         servico = 'Aluguel';
     }else{
         servico = 'Venda';
     }
    let condicao;
-   if(autoVeiculo.quilometragem == 0){
+   if(autoVeiculo.quilometragem === 0){
     condicao = 'Novo';
    } else {
     condicao = 'Usado';
    }
    let status;
-   if(autoVeiculo.status == true){
+   if(autoVeiculo.servico === true){
     status = 'Disponivel';
    } else {
     status = 'Indisponível';
@@ -63,18 +30,20 @@ export default function informacoesVeiculos(){
 
     return(
         <div className={styles.informacoesVeiculos}>
-            <h1>{`${autoModelo.model} ${autoModelo.marca}`} </h1>
-            <p> Serviço: {`${servico}`}</p>
-            <p>Condição: {`${condicao}`}</p>
-            <p>Status: {`${status}`}</p>
-            <p>Placa: {`${autoVeiculo.placa}`}</p>
-            <p>Preço: {`${preco}`}</p>
-            <p>Quilometragem: {`${autoVeiculo.quilometragem}`}</p>
-            <p>Cor: {`${autoVeiculo.cor}`}</p>
-            <p>Dono: {`${donoAuto.first_name}`}</p>
             <div>
-                <button id='Editar'>Editar</button>
-                <button id='Deletar'>Deletar</button>
+                <h1>{`${autoModelo.model} ${autoModelo.marca}`} </h1>
+                <p> Serviço: {`${servico}`}</p>
+                <p>Condição: {`${condicao}`}</p>
+                <p>Status: {`${status}`}</p>
+                <p>Placa: {`${autoVeiculo.placa}`}</p>
+                <p>Preço: {`${preco}`}</p>
+                <p>Quilometragem: {`${autoVeiculo.quilometragem}`}</p>
+                <p>Cor: {`${autoVeiculo.cor}`}</p>
+                <p>Dono: {`${donoAuto.first_name}`}</p>
+                <div>
+                    <button id='Editar'>Editar</button>
+                    <button id='Deletar'>Deletar</button>
+                </div>
             </div>
         </div>
     );
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,7 +2,7 @@ import Rodape from '../../components/rodape'
 import Cabecalho from '../../components/cabecalho'
 import BarraLateral from '../../components/barraLateralAdmin'
 import FiltroVeiculos from '../../components/FiltroVeiculos'
-import InformacoesVeiculos from '../../components/informacoesVeiculo'
+import RenderinformacoesVeiculos from '../../components/renderizaInformacoesVeiculos'
 
 
 import Styles from '../../styles/adminHome.module.css'
@@ -10,14 +10,19 @@ import Styles from '../../styles/adminHome.module.css'
 export default function adminHome() {
   return (
     <div className={Styles.adminHome}> 
-        <Cabecalho />
-        <div>
-            <BarraLateral/>
-            <FiltroVeiculos />
-          <InformacoesVeiculos />
+          <Cabecalho />
+        <div className={Styles.Barra}>
+          <div className={Styles.propBarra}>
+          <BarraLateral/>
+          <FiltroVeiculos />
+          </div>
+          <div className={Styles.conteudo}>
+            <RenderinformacoesVeiculos />
+          </div>
+          
+
         </div>
         
-      
         <Rodape/>
     </div>
       )
