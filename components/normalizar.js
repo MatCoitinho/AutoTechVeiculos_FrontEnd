230c76997d7f8a408f9fbd8e6030f3ae5d9e3f21
unbigou <111555933+unbigou@users.noreply.github.com>
2023-10-27 23:39:18 -0300

    fix: deleting old stuff

--- components/normalizar.js
+++
@@ -1,8 +0,0 @@
-export default function normalizar() {
-    return(
-        <header>
-            <link href='../styles/normalize.css' rel='stylesheet'></link>
-            <link href='../styles/reset.css' rel='stylesheet'></link>
-        </header>
-    )
-}
--- pages/lib/api.js
+++
@@ -1,7 +0,0 @@
-import Axios from 'axios'
-
-const api = Axios.create({
-    baseURL: 'http://127.0.0.1:8000/'
-})
-
-export default api;
