7c5b56f361a256136dfb21a5f7de22825847dec7
jpAndrade23 <joaopedro917382@gmail.com>
2023-10-29 22:47:56 -0300

    feat: criação do metodo de get de veiculos

--- src/app/admin/anuncios/page.tsx
+++ src/app/admin/anuncios/page.tsx
@@ -5,17 +5,56 @@ import { Search } from 'lucide-react'
 import SideBarAdmin from '../../../components/ui/sideBarAdmin'
 import { useRouter } from "next/navigation";
 
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+
+import { getAnuncio } from '@/app/api/getAnuncio';
+
+
+
+
+
 
 export default function anuncios(){
    
+
+    const [modelsComponents, setModelsComponents] = useState<JSX.Element[]>([]);
+
+    useEffect(() => {
+        async function getdados() {
+            const anuncios1 = await getAnuncio('');
+        
+            try{ 
+                if(!anuncios1) throw('error')
+                let value = anuncios1.data
+                setModelsComponents(value.map((anuncio) => (
+                    
+                    <div key={anuncio.id} className='border  border-slate-950 p-2 w-fit rounded-md hover:scale-105'>
+                        <h2 className='text-center font-bold'>{anuncio.modelo}</h2>
+                        <p>Marca: {anuncio.marca}</p>
+                        <p>Serviço: {anuncio.servico == true? 'Venda':'Aluguel'}</p>
+                        <p>Destaque: {anuncio.destaque == true? 'Ativo':'Inativo'}</p>
+                        <p>Pontos: {anuncio.pontos}</p>
+                        <p>Preço: {anuncio.preco}</p>
+                        <div className='flex   justify-between mt-4'>
+                            <button  className='p-2  bg-zinc-300 rounded-md m-1' onClick={() => editar('modelo', String(anuncio.id))} type='button'>Editar</button>
+                            <button  className='p-2  bg-zinc-300  rounded-md m-1' type='button'>Deletar</button>
+                        </div>
+                    </div>
+                )));
+            }  catch{
+                setModelsComponents([<p className='text-red-500 text-center'>nenhum item encontrado</p>]);
+            }
+        }
+
+        getdados();
+    }, []);
+
+    
 
     const {push} = useRouter();
     const editar =(classe: string, parametro: string) => {
         push(`/admin/editar/${classe}/${parametro}`)
     }
-
-
 
     const [busca, setBusca] = useState('')
     const setValueSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,53 +67,7 @@ export default function anuncios(){
     }
 
 
-    const anuncios1 = 
-    [
-        {
-            "id": 1,
-            "modelo": "Onix",
-            "marca": "Chevrolet",
-            "cambio": "True",
-            "ano": "2019",
-            "combustivel": "gasolina",
-            "placa": "mio1234",
-            "cor": "Preto",
-            "categoria": "sedan",
-            "status": "True",
-            "pontos": 0,
-            "img1": "http://127.0.0.1:8000/media/mio1234-3afdec86-2120-4a6c-8a9b-51801451cfca.jfif",
-            "img2": "http://127.0.0.1:8000/media/mio1234-1611922101_Screenshot_20210128-230452-1.jpg",
-            "descricao": "nada de mais",
-            "destaque": true,
-            "preco": 100000,
-            "servico": true,
-            "veiculo": 1
-        }
-        
-        
-    ]
-
-    let modelsComponents;
-    try{ 
-        if(anuncios1.length<1) throw('error');
-        modelsComponents = anuncios1.map((anuncio, index) => (
-            <div key={index} className='border  border-slate-950 p-2 w-fit rounded-md hover:scale-105'>
-                <h2 className='text-center font-bold'>{anuncio.modelo}</h2>
-                <p>Marca: {anuncio.marca}</p>
-                <p>Serviço: {anuncio.servico == true? 'Venda':'Aluguel'}</p>
-                <p>Destaque: {anuncio.destaque == true? 'Ativo':'Inativo'}</p>
-                <p>Pontos: {anuncio.pontos}</p>
-                <p>Preço: {anuncio.preco}</p>
-                <div className='flex   justify-between mt-4'>
-                    <button  className='p-2  bg-zinc-300 rounded-md m-1' onClick={() => editar('modelo', String(anuncio.id))} type='button'>Editar</button>
-                    <button  className='p-2  bg-zinc-300  rounded-md m-1' type='button'>Deletar</button>
-                </div>
-            </div>
-    ))
-    } catch{
-        modelsComponents =<p className='  text-red-500 text-center'>nenhum item encontrado</p> 
-
-    }
+ 
 
 
 
